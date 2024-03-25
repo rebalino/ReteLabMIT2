a6f60b56f04136efa791f31294c8183fb31aa2fc
rebalino <zsalabnagy@gmail.com>
2024-03-25 18:45:53 +0100

    Tests done

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainUser.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainUser.java
@@ -7,5 +7,8 @@ public interface TrainUser {
 	boolean getAlarmFlag();
 
 	void overrideJoystickPosition(int joystickPosition);
+	
+	boolean getAlarmState();
 
+	void setAlarmState(boolean alarmState);
 }
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -9,16 +9,45 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import java.time.LocalTime;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 public class TrainSensorTest {
-   /*
+    private TrainController mockController;
+    private TrainUser mockUser;
+    private TrainSensor sensor;
 
     @Before
-    public void before() {
-        
+    public void init(){
+        mockController = mock(TrainController.class);
+        mockUser = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(mockController, mockUser);
     }
 
     @Test
-    public void TableRowAddedTest() {
-     
-    }*/
+    public void OverAbsoluteSpeedLimit(){
+        sensor.overrideSpeedLimit(600);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void InAbsoluteSpeedLimit(){
+        sensor.overrideSpeedLimit(250);
+        verify(mockUser, times(1)).setAlarmState(false);
+    }
+    
+    @Test
+    public void OverRelativeSpeedLimit(){
+        when(mockController.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(50);
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void InRelativeSpeedLimit(){
+        when(mockController.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(100);
+        verify(mockUser, times(1)).setAlarmState(false);
+    }
 }
